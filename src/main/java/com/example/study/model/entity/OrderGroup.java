eaebdc1325a7ef02b9c90dc0f6ee906d3bfc2f89
nahap <nahappy15@naver.com>
2021-01-21 00:27:32 +0900

    OrderGroup Entity 변수 수정 OrderGroup, AdminUser의 create test

--- src/main/java/com/example/study/model/entity/OrderGroup.java
+++ src/main/java/com/example/study/model/entity/OrderGroup.java
@@ -45,4 +45,6 @@ public class OrderGroup {
     private LocalDateTime updatedAt;
 
     private String updatedBy;
+
+    private Long userId;
 }
---
+++ src/test/java/com/example/study/repository/OrderGroupRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.OrderGroup;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class OrderGroupRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    private OrderGroupRepository orderGroupRepository;
+
+    @Test
+    public void create(){
+        OrderGroup orderGroup = new OrderGroup();
+        orderGroup.setStatus("COMPLETE");
+        orderGroup.setOrderType("CARD");
+        orderGroup.setRevAddress("seoul gangnam");
+        orderGroup.setRevName("홍길");
+        orderGroup.setPaymentType("Card");
+        orderGroup.setTotalPrice(BigDecimal.valueOf(900000));
+        orderGroup.setTotalQuantity(1);
+        orderGroup.setOrderedAt(LocalDateTime.now().minusDays(2));
+        orderGroup.setCreatedAt(LocalDateTime.now());
+        orderGroup.setCreatedAt(LocalDateTime.now());
+        orderGroup.setCreatedBy("AdminServer");
+        orderGroup.setUserId(1L);
+
+        OrderGroup newOrderGroup =  orderGroupRepository.save(orderGroup);
+
+
+    }
+}
